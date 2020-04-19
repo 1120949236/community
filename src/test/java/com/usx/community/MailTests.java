c02bac7d5e076feeb414fa2a1f9edb2ee569dec4
lyf <1120949236@qq.com>
2020-04-19 23:04:03 +0800

    注册功能提交

---
+++ src/test/java/com/usx/community/MailTests.java
@@ -0,0 +1,40 @@
+package com.usx.community;
+
+import com.usx.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("1033179514@qq.com", "TEST", "测试邮件");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("1033179514@qq.com","HTML",content);
+    }
+
+}
