2cec84d61bba1f7f015ca48070cb1b8875a5018a
lyf <1120949236@qq.com>
2020-04-20 22:31:07 +0800

    登录功能初步

--- src/main/java/com/usx/community/controller/HomeController.java
+++ src/main/java/com/usx/community/controller/HomeController.java
@@ -32,19 +32,18 @@ public class HomeController {
         page.setRows(discussPostService.findDiscussPostRows(0));
         page.setPath("/index");
 
-        List<DiscussPost> list = discussPostService.findDiscussPosts(0,page.getOffset(),page.getLimit());
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
         List<Map<String, Object>> discussPosts = new ArrayList<>();
-        if(list != null){
-            for (DiscussPost post: list){
+        if (list != null) {
+            for (DiscussPost post : list) {
                 Map<String, Object> map = new HashMap<>();
                 map.put("post", post);
                 User user = userService.findUserById(post.getUserId());
-                map.put("user",user);
+                map.put("user", user);
                 discussPosts.add(map);
             }
         }
         model.addAttribute("discussPosts", discussPosts);
         return "/index";
     }
-
 }
