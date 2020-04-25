ee588afbee57dd19fee4063e13e39106947685e6
lyf <1120949236@qq.com>
2020-04-26 00:28:21 +0800

    点赞与关注功能

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -6,9 +6,38 @@ function follow() {
 	var btn = this;
 	if($(btn).hasClass("btn-info")) {
 		// 关注TA
-		$(btn).text("已关注").removeClass("btn-info").addClass("btn-secondary");
+		$.post(
+		    CONTEXT_PATH + "/follow",
+            {"entityType":3,"entityId":$(btn).prev().val()},
+            function (data) {
+		        data = $.parseJSON(data);
+		        if (data.code==0) {
+                    $("#hintBody").text("已成功关注!");
+                } else {
+                    $("#hintBody").text(data.msg);
+                }
+            }
+        );
+		// $(btn).text("已关注").removeClass("btn-info").addClass("btn-secondary");
 	} else {
 		// 取消关注
-		$(btn).text("关注TA").removeClass("btn-secondary").addClass("btn-info");
+        $.post(
+            CONTEXT_PATH + "/unfollow",
+            {"entityType":3,"entityId":$(btn).prev().val()},
+            function (data) {
+                data = $.parseJSON(data);
+                if (data.code==0) {
+                    $("#hintBody").text("已取消关注！");
+                } else {
+                    $("#hintBody").text(data.msg);
+                }
+            }
+        );
+		// $(btn).text("关注TA").removeClass("btn-secondary").addClass("btn-info");
 	}
+    $("#hintModal").modal("show");
+    setTimeout(function(){
+        $("#hintModal").modal("hide");
+        location.reload();
+    }, 1000);
 }
