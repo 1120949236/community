82425d30173477bf579813541c595b114c3ef17a
lyf <1120949236@qq.com>
2020-04-29 00:02:42 +0800

    置顶加精删除

---
+++ src/main/java/com/usx/community/dao/elasticsearch/DiscussPostRepository.java
@@ -0,0 +1,11 @@
+package com.usx.community.dao.elasticsearch;
+
+import com.usx.community.entity.DiscussPost;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
+
+
+}
